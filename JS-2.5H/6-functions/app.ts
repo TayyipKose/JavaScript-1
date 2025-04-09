903b3e96eb46f6489e4ffb1da4c4915a443487a2
Tayyip Kose <tayyipkose037@gmail.com>
2025-04-09 11:29:24 +0300

    callback-hof small edit

--- JS-2.5H/6-functions/app.ts
+++ JS-2.5H/6-functions/app.ts
@@ -2,18 +2,21 @@
 function greet(): void {
     console.log("Merhaba!");
 }
+
 greet(); // Çıktı: Merhaba!
 
 // 2️⃣ Parametre Alan Fonksiyon (Geri Dönüşsüz)
 function greetUser(name: string): void {
     console.log(`Merhaba, ${name}!`);
 }
+
 greetUser("Ali"); // Çıktı: Merhaba, Ali!
 
 // 3️⃣ Değer Döndüren Fonksiyon (return ile)
 function getGreeting(name: string): string {
     return `Merhaba, ${name}!`; // Değeri dışarı döndürür
 }
+
 const mesaj = getGreeting("Zeynep");
 console.log(mesaj); // Çıktı: Merhaba, Zeynep!
 
@@ -22,9 +25,9 @@ console.log(mesaj); // Çıktı: Merhaba, Zeynep!
 function sum(a: number, b: number): number {
     return a + b; // Hesap sonucu döner
 }
+
 const toplam = sum(5, 10);
 console.log("Toplam:", toplam); // Çıktı: Toplam: 15
-
 
 
 // 5️⃣ Callback Fonksiyon (Fonksiyon parametre olarak alma)
@@ -32,11 +35,12 @@ function processData(data: number, callback: (result: number) => void): void {
     const islenmisVeri = data * 2;
     callback(islenmisVeri);
 }
+
+processData(10, printResult); // Çıktı: Sonuç: 20
+
 function printResult(result: number): void {
     console.log("Sonuç:", result);
 }
-processData(10, printResult); // Çıktı: Sonuç: 20
-
 
 
 // 6️⃣ Fonksiyon Döndüren Fonksiyon (Higher Order Function)
@@ -45,8 +49,17 @@ function createMultiplier(multiplier: number): (value: number) => number {
         return value * multiplier;
     };
 }
+
 const double = createMultiplier(2);
 console.log(double(5)); // Çıktı: 10
 
 const triple = createMultiplier(3);
 console.log(triple(5)); // Çıktı: 15
+
+
+/*
+Callback-HOF Farkı
+Callback Fonksiyonu: Başka bir fonksiyonun parametre olarak aldığı, işlem tamamlandığında çalıştırılan fonksiyondur.
+Higher-Order Function (HOF): Bir fonksiyonun başka bir fonksiyonu parametre olarak alması veya bir fonksiyon döndürmesidir.
+Özetle her callback, aslında bir hoftur.
+* */
